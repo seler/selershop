73fe773d99fdcb9f9d5aa360962c7707deb6a8cf
Rafał Selewońko <rafal@selewonko.com>
2011-06-02 23:34:56 +0200

    początek

---
+++ src/selershop/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selershop;
+
+/**
+ *
+ * @author Seler
+ */
+public class NewClass {
+    
+}
